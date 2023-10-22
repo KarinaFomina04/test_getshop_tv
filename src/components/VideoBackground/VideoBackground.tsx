a46e3105d5a1c3f989e3923d8f78d7d205956b6c
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-10-23 00:38:10 +0200

    add function toggleVideo  in  Banner.tsx

--- src/components/VideoBackground/VideoBackground.tsx
+++ src/components/VideoBackground/VideoBackground.tsx
@@ -1,25 +1,38 @@
-import { useEffect, useState } from 'react'
+import {useEffect, useRef, useState} from 'react'
 import videoBg from '../../assets/video/videoBg.mp4'
 import s from './VideoBackground.module.scss'
 import { Banner } from '../Banner'
 
 export const VideoBackground = () => {
-    const [showQrCode, setShowQrCode] = useState(false);
+    const [showQrCode, setShowQrCode] = useState(false)
+    const [isPlaying, setIsPlaying] = useState(true)
+    const videoRef = useRef<HTMLVideoElement | null>(null)
+
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            setShowQrCode(true);
-        }, 5000);
+            setShowQrCode(true)
+        }, 5000)
 
-        return () => clearTimeout(timer);
-    }, []);
+        return () => clearTimeout(timer)
+    }, [])
 
+    const toggleVideo = () => {
+        if (videoRef.current) {
+            if (isPlaying) {
+                videoRef.current.pause()
+            } else {
+                videoRef.current.play()
+            }
+            setIsPlaying(!isPlaying)
+        }
+    }
     return (
-        <div className={s.videoBackground}>
-            <video src={videoBg} autoPlay loop muted />
+        <div onClick={toggleVideo} className={s.videoBackground}>
+            <video ref={videoRef} src={videoBg} autoPlay loop muted />
             <div className={s.content}>
                 {showQrCode && <Banner />}
             </div>
         </div>
-    );
-};
+    )
+}
